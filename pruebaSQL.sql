2ae51b9edacba1bcc662a88bf1140b5eaf7449c2
Edward Bustos <esbgo97@gmail.com>
2023-07-20 11:26:22 -0500

    Punto 2: vista de vehiculos nuevos

--- pruebaSQL.sql
+++ pruebaSQL.sql
@@ -13,3 +13,12 @@ insert  into vehiculos (marca, modelo, descripcion) values ('BMW', 1997, 'E46');
 insert  into vehiculos (marca, modelo, descripcion) values ('Mazda', 2022, 'Mazda 3');
 insert  into vehiculos (marca, modelo, descripcion) values ('Toyota', 2023, 'Supra MK5');
 
+
+--Punto 1. Crear vista de vehiculos nuevos
+create view vehiculos_nuevos as
+	select * from vehiculos
+	where modelo >= extract (year from now()) - 5
+	order by marca asc;
+
+select * from vehiculos_nuevos 
+
