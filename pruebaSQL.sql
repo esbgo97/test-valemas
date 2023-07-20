8b27bd1ce73f8a342929c3cb74a344fc81c32da4
Edward Bustos <esbgo97@gmail.com>
2023-07-20 11:21:23 -0500

    punto 1: crear y poblar tabla vehiculos

---
+++ pruebaSQL.sql
@@ -0,0 +1,15 @@
+--Punto 1. Crear y poblar tabla vehculos
+create table vehiculos (
+	id SERIAL primary KEY,
+	marca text,
+	modelo int,
+	descripcion text
+);
+
+
+insert  into vehiculos (marca, modelo, descripcion) values ('Renault', 2017, 'Sandero RS');
+insert  into vehiculos (marca, modelo, descripcion) values ('Ford', 2015, 'Fista ST');
+insert  into vehiculos (marca, modelo, descripcion) values ('BMW', 1997, 'E46');
+insert  into vehiculos (marca, modelo, descripcion) values ('Mazda', 2022, 'Mazda 3');
+insert  into vehiculos (marca, modelo, descripcion) values ('Toyota', 2023, 'Supra MK5');
+
